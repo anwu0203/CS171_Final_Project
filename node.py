1a60bd8dac120b1b91e60059010d24cfa6a9fd04
Jesus Oviedo <jaoviedo@ucsb.edu>
2023-06-10 15:59:24 -0700

    Updates to add ballot num and blog app

--- node.py
+++ node.py
@@ -10,153 +10,238 @@ from time import sleep
 import hashlib
 import argparse
 
+from utils import BallotNum
+from utils import PriorityQueue
+from utils import Blockchain
 
-class BlogChain:
-    def _init_(self):
-        self._chain = []
-        self._head = -1
+class BlogChain(Blockchain):
+	# Mostly for debugging, should use other init everytime with proper implementation
+	def __init__(self):
+		# Nonce string to mine for
+		difficulty = '000' + '1'*253
 
+		# Initialize the blockchain to mine for this diff.
+		super.__init__(difficulty)
+
+		# Where to append our committed transactions to, our backup file for testing
+		try:
+			self.backup_writer = open('/saves/backup_0.txt', 'a')
+		except FileNotFoundError:
+			# Create a new file
+			print('The backup file provided does not exist')
+	
+	# Initialization for restoring the blockchain from failure
+	def __init__(self, difficulty, backup_file_location):
+		# Nonce string to mine fordifficulty = '000' + '1'*253
+
+		# Initialize the blockchain to mine for this diff.
+		super.__init__(difficulty)
+
+		# Am I up-to-date?
+		self.valid = 0
+
+		# Read the file from top to bottom (first transaction to last) 
+		# And replicate the blockchain last stored in the backup file
+		# By simply adding the transactions in order
+		with open(backup_file_location, 'r') as reader:
+			pass
+		
+		# Once we have loaded our backup, make sure we can we append to it
+		try:
+			self.backup_writer = open(backup_file_location, 'a')
+		except FileNotFoundError:
+			# Backup file did not exist, check if folder location is valid and create a new file with that backup name
+			# If the folder location does not exist, fail gracefully?
+			print('The backup file provided does not exist')
+
+	# Function to call when we are up to date with our depth
+	def validate(self):
+		self.valid = 1
+	
+	# Must overwrite to adapt to the blog app
+	# This function is ran before mining is attempted for the post/comment to make sure we are not committing anything we shouldn't
+	# Returns:
+	#  * True if valid transaction (If it is a post with a username and title -OR- a the post exist to comment on)
+	#  * False otherwise
+	def valid_transaction(self, transaction):
+		return False
 
 def connect():
-    sleep(3)
-    for portID, port in PORTS.items():
-        if (portID != PID):
-            try:
-                out_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                out_sock.connect((IP, port))
-                raddr = out_sock.getpeername()
-                out_socks[portID] = (out_sock, raddr)
-                print(f"connected to {raddr[1]}", flush=True)
-            except:
-                print("exception in trying to connect to server", flush=True)
-    sleep(1)
-    for sockID, out_sock in out_socks.items():
-        try:
-            out_sock[0].sendall(bytes('Hi ' + PID, "utf-8"))
-        except:
-            print("exception in sending to server", flush=True)
+	sleep(3)
+	for portID, port in PORTS.items():
+		if (portID != PID):
+			try:
+				out_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				out_sock.connect((IP, port))
+				raddr = out_sock.getpeername()
+				out_socks[portID] = (out_sock, raddr)
+				print(f"connected to {raddr[1]}", flush=True)
+			except:
+				print("exception in trying to connect to server", flush=True)
+	sleep(1)
+	for sockID, out_sock in out_socks.items():
+		try:
+			out_sock[0].sendall(bytes('Hi ' + PID, "utf-8"))
+		except:
+			print("exception in sending to server", flush=True)
 
-    
+	
 
 def get_user_input():
-    while True:
-        user_input = input()
-        if user_input == 'exit':
-            # exit and close all connections
-            for sockID, out_sock in out_socks.items():
-                out_sock[0].close()
-            stdout.flush()
-            _exit(0)
-        elif user_input == 'connect':
-            threading.Thread(target=connect).start()
-        elif user_input == 'print':
-            for sockID, out_sock in out_socks.items():
-                print(f'Outsock ID: {sockID} conn: {out_sock[0]} addr: {out_sock[1]}', flush=True)
-            for sockID, in_sock in in_socks.items():
-                print(f'Insock ID: {sockID} conn: {in_sock[0]} addr: {in_sock[1]}', flush=True)
-        else:
-            for sockID, out_sock in out_socks.items():
-                try:
-                    out_sock[0].sendall(bytes(user_input, "utf-8"))
-                except:
-                    print("exception in sending to server", flush=True)
+	while True:
+		user_input = input()
+		# ********** Operator Commands **********
+		if user_input == 'exit':
+			# exit and close all connections
+			for sockID, out_sock in out_socks.items():
+				out_sock[0].close()
+			stdout.flush()
+			_exit(0)
+		elif user_input == 'connect':
+			threading.Thread(target=connect).start()
+		elif user_input == 'print':
+			for sockID, out_sock in out_socks.items():
+				print(f'Outsock ID: {sockID} conn: {out_sock[0]} addr: {out_sock[1]}', flush=True)
+			for sockID, in_sock in in_socks.items():
+				print(f'Insock ID: {sockID} conn: {in_sock[0]} addr: {in_sock[1]}', flush=True)
+		# ********** Protocol Commands **********
+		elif user_input == 'crash':
+			pass
+		elif user_input == 'failLink':
+			pass
+		elif user_input == 'fixLink':
+			pass
+		elif user_input == 'blockchain':
+			pass
+		elif user_input == 'queue':
+			pass
+		# ********** Application Commands **********
+		elif user_input == 'post':
+			pass
+			# 
+		elif user_input == 'comment':
+			pass
+		elif user_input == 'blog':
+			pass
+		elif user_input == 'view':
+			pass
+		elif user_input == 'read':
+			pass
+		else:
+			for sockID, out_sock in out_socks.items():
+				try:
+					out_sock[0].sendall(bytes(user_input, "utf-8"))
+				except:
+					print("exception in sending to server", flush=True)
 
 
 def respond(conn, raddr):
-    
-    # listen for incoming messages
-    while True:
-        try:
-            data = conn.recv(1024)
-        except:
-            print(f"exception in receiving data from {raddr[1]}", flush=True)
-            break
-        if not data:
-            # close connection to node
-            conn.close()
-            try:
-                out_socks.pop(pid)
-            except:
-                print(f"exception in disconnecting from {raddr[1]}", flush=True)
-            try:
-                in_socks.pop(pid)
-            except:
-                print(f"exception in disconnecting from {raddr[1]}", flush=True)
-            print(f"connection closed from {raddr[1]}", flush=True)
-            break
+	
+	# listen for incoming messages
+	while True:
+		try:
+			data = conn.recv(1024)
+		except:
+			print(f"exception in receiving data from {raddr[1]}", flush=True)
+			break
+		if not data:
+			# close connection to node
+			conn.close()
+			try:
+				out_socks.pop(pid)
+			except:
+				print(f"exception in disconnecting from {raddr[1]}", flush=True)
+			try:
+				in_socks.pop(pid)
+			except:
+				print(f"exception in disconnecting from {raddr[1]}", flush=True)
+			print(f"connection closed from {raddr[1]}", flush=True)
+			break
 
-        data = data.decode()
-        print(data)
-        if data.startswith('Hi'):
-            pid = data[3:]
-            for portID, port in PORTS.items():
-                if pid == portID:
-                    in_socks[portID] = (conn, raddr)
-                    break
+		data = data.decode()
+		print(data)
+		if data.startswith('Hi'):
+			pid = data[3:]
+			for portID, port in PORTS.items():
+				if pid == portID:
+					in_socks[portID] = (conn, raddr)
+					break
 
-            # test outward connection to target node
-            sleep(3)
-            try:
-                out_socks[pid][0].sendall(bytes("connection check", "utf-8"))
-                print("check success", flush=True)
-            except:
-                print(f"check failed {pid}, {port}", flush=True)
-                try:
-                    out_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    out_sock.connect((IP, port))
-                    raddr = out_sock.getpeername()
-                    out_socks[pid] = (out_sock, raddr)
-                    print(f"connected to {raddr[1]}", flush=True)
-                except:
-                    print("exception in trying to connect to server", flush=True)
-                sleep(1)
-                for sockID, out_sock in out_socks.items():
-                    try:
-                        out_sock[0].sendall(bytes('Hi ' + PID, "utf-8"))
-                    except:
-                        print("exception in sending to server", flush=True)
-            
+			# test outward connection to target node
+			sleep(3)
+			try:
+				out_socks[pid][0].sendall(bytes("connection check", "utf-8"))
+				print("check success", flush=True)
+			except:
+				print(f"check failed {pid}, {port}", flush=True)
+				try:
+					out_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					out_sock.connect((IP, port))
+					raddr = out_sock.getpeername()
+					out_socks[pid] = (out_sock, raddr)
+					print(f"connected to {raddr[1]}", flush=True)
+				except:
+					print("exception in trying to connect to server", flush=True)
+				sleep(1)
+				for sockID, out_sock in out_socks.items():
+					try:
+						out_sock[0].sendall(bytes('Hi ' + PID, "utf-8"))
+					except:
+						print("exception in sending to server", flush=True)
+			
 
 
 if __name__ == "__main__":
 
-    sleep(1)
+	sleep(1)
 
-    # get command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('pid', type=str, help='Node ID')
-    args = parser.parse_args()
+	# get command line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('pid', type=str, help='Node ID')
+	args = parser.parse_args()
 
-    # get PID, server IP, and node port number
-    PID = args.pid
-    IP = socket.gethostname()
-    PORTS = {'P1':9291, 'P2':9292, 'P3':9293, 'P4':9294, 'P5':9295}
-    # PORTS = [9291, 9292, 9293, 9294, 9295]
-    PORT = PORTS[PID]
-    
-    # open listening socket
-    in_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    in_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    in_sock.bind((IP, PORT))
-    in_sock.listen()
+	# get PID, server IP, and node port number
+	PID = args.pid
+	IP = socket.gethostname()
+	PORTS = {'P1':9291, 'P2':9292, 'P3':9293, 'P4':9294, 'P5':9295}
+	LEADER = []
+	# PORTS = [9291, 9292, 9293, 9294, 9295]
+	PORT = PORTS[PID]
+	
+	# open listening socket
+	in_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	in_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	in_sock.bind((IP, PORT))
+	in_sock.listen()
 
-    # containers for connections
-    out_socks = {}
-    in_socks = {}
+	# containers for connections
+	out_socks = {}
+	in_socks = {}
 
-    global missing_replies
-    missing_replies = [5]
+	global missing_replies
+	missing_replies = [5]
 
-    # start thread for user input
-    threading.Thread(target=get_user_input).start()
-    threading.Thread(target=connect).start()
+	NONCE_STRING = '000' + '1'*253
 
-    # receive incoming connections
-    while True:
-        try:
-            conn, addr = in_sock.accept()
-            print(f"accepted connection from {addr[1]}", flush=True)
-        except:
-            print("exception in accepting incoming connection", flush=True)
-            break
-        threading.Thread(target=respond, args=(conn, addr)).start()
+	# Server Blockchain
+	local_blog = BlogChain() # BlogChain(NONCE_STRING, 'backup/file/location.txt')
+	
+	# Server Ballot Number, contains local time and depth of our current blockchain
+	ballot_num = BallotNum(PID)
+
+	# Server Request Queue
+	request_queue = PriorityQueue()
+
+	# start thread for user input
+	threading.Thread(target=get_user_input).start()
+	threading.Thread(target=connect).start()
+
+	# receive incoming connections
+	while True:
+		try:
+			conn, addr = in_sock.accept()
+			print(f"accepted connection from {addr[1]}", flush=True)
+		except:
+			print("exception in accepting incoming connection", flush=True)
+			break
+		threading.Thread(target=respond, args=(conn, addr)).start()
---
+++ utils.py
@@ -0,0 +1,201 @@
+import hashlib
+
+
+
+class PriorityQueue(object):
+	def __init__(self):
+		self.queue = []
+		
+	def __str__(self):
+		return ' '.join([str(i) for i in self.queue])
+ 
+	# for checking if the queue is empty
+	def isEmpty(self):
+		return len(self.queue) == 0
+ 
+	# for inserting an element in the queue
+	def insert(self, data):
+		self.queue.append(data)
+
+	# for popping an element based on Priority
+	def delete(self):
+		try:
+			max_ind = 0
+			for i in range(len(self.queue)):
+				if self.queue[i] < self.queue[max_ind]:
+					max_ind = i
+			item = self.queue[max_ind]
+			del self.queue[max_ind]
+			return item
+		except IndexError:
+			print()
+	
+	def top(self):
+		try:
+			max_val = 0
+			for i in range(len(self.queue)):
+				if self.queue[i] < self.queue[max_val]:
+					max_val = i
+			item = self.queue[max_val]
+			return item
+		except IndexError:
+			print()
+
+class LamportClock:
+	def __init__(self, pid):
+		self.time = 0
+		self.pid = pid
+
+	def __str__(self):
+		return f'{self.time} {self.pid}'
+
+	def __repr__(self):
+		return f'({self.time} {self.pid})'
+
+	def inc_time(self):
+		self.time += 1
+		print("Local Time:", self.time)
+
+	def sync(self, recv_clock):
+		self.time = max(int((recv_clock).split(' ')[0]), self.time)
+		self.inc_time()
+	
+class BallotNum(LamportClock):
+	def __init__(self, pid):
+		super().__init__(pid)
+		# Depth I am aware of as a node
+		self.depth = 0 
+	# Call when a node commits a block
+	def inc_depth(self):
+		self.depth += 1
+		print("Local Depth:", self.depth)
+	def __str__(self):
+		return f'{self.time} {self.pid} {self.depth}'
+
+	def __repr__(self):
+		return f'({self.time}, {self.pid}, {self.depth})'
+
+class HashPointer:
+	def __init__(self, index, hash_prev):
+		self.index = index
+		self.hash_prev = hash_prev
+	def get_hash(self):
+		return self.hash_prev
+	def get_index(self):
+		return self.index
+
+class HashBlock:
+	def __init__(self, hash_ptr, transaction = None, nonce = None, llc = None):
+		# Hash Pointer to the previous block
+		self.hash_ptr = hash_ptr
+		# String to be treated as the transaction
+		self.transaction = transaction
+		# Calculated nonce less than nonce string < NONCE_STRING
+		self.nonce = nonce#
+		# Ballot Number / Timestamp associated with the transaction
+		self.llc = llc
+	# String representation of the hashblock
+	def __str__(self):
+		return f'{self.hash_ptr.get_hash()}{str(self.transaction)}{str(self.nonce)}'
+	
+	# Formatted representation of the hashblock with extra information not in the block (i.e. timestamp)
+	def __repr__(self):
+		parsed = self.transaction.split('$')
+		sender = parsed[0][:2]
+		recepient = parsed[0][2:]
+		amount = parsed[1]
+		clock = self.llc.split(' ')
+		return f'({sender}, {recepient}, ${amount}, {clock[0]}.{clock[1]}{clock[2]}, {self.hash_ptr.get_hash()})'
+	
+	# Getter for transaction found on this block (post or comment)
+	def get_transaction(self):
+		return self.transaction
+	
+	# Get the pointer (index of the previous block) to be able to access the previous hash block
+	def get_prev_ptr(self):
+		return self.hash_ptr.get_index()
+
+class Blockchain:
+	def __init__(self, difficulty):
+		self._chain = []
+		self._head = -1
+		self.NONCE_STRING = difficulty
+
+	def __str__(self):
+		bc = "["
+		i = 0
+		while i <= self._head:
+			bc += repr(self._chain[i])
+			if i < self._head:
+				bc += ", "
+			i += 1
+		bc += "]"
+		return bc
+	
+	def append_block(self, transaction, llc):
+		if self.valid_transaction(transaction):
+			if len(self._chain) == 0: # This is our Genesis (First) Block
+				hash_prev = (hex(0)[2:].zfill(64))
+				h_ptr = HashPointer(-1, hash_prev)
+				nonce = self.mine(hash_prev, transaction)
+				self._chain.append(HashBlock(h_ptr, transaction, nonce, llc))
+				self._head += 1		
+			else: # This is not the first transaction, must verify the transaction is valid before appending 
+				index = self._head # The index of the previous block
+				hash_input = str(self._chain[index])
+				hash_prev = hashlib.sha256(hash_input.encode('UTF-8')).hexdigest()
+				h_ptr = HashPointer(index, hash_prev)
+				nonce = self.mine(hash_prev, transaction)
+				self._chain.append(HashBlock(h_ptr, transaction, nonce, llc))
+				self._head += 1
+			return True
+		else:
+			return False
+
+	def sha256(self, str_in):
+		hex_str = hashlib.sha256(str_in.encode('UTF-8')).hexdigest()
+		return bin(int(hex_str,16))[2:].zfill(256)
+	
+	def mine(self, h_prev, trans):
+		nonce = 0
+		mining = True
+		while mining:
+			attempt = str(h_prev) + str(trans) + str(nonce)
+			attempt = self.sha256(attempt)
+			if self.valid_nonce(attempt):
+				mining = False
+				break
+			nonce += 1
+		return nonce
+	
+	def valid_nonce(self, attempt):
+		if attempt < self.NONCE_STRING:
+			return True
+		return False
+	
+	def valid_transaction(self, transaction):
+		return False
+	
+	# def valid_transaction(self, transaction):
+	# 	sender = transaction[:2]
+	# 	amount = int(transaction.split('$')[1])
+	# 	sender_balance = int(self.balance(sender)[1:])
+	# 	# print(sender, sender_balance, amount)
+	# 	if (sender_balance - amount) < 0: # "Insufficient Balance"
+	# 		return False
+	# 	return True
+	
+	# def balance(self, client):
+	# 	total = 10
+	# 	# Traverse blockchain
+	# 	i = self._head
+	# 	while i >= 0:
+	# 		# print("Traversing for:", client)
+	# 		curr_transaction = self._chain[i].get_transaction()
+	# 		client_pos = curr_transaction.find(client)
+	# 		if client_pos == 0: # This transaction is an expense from the client
+	# 			total -= int(curr_transaction.split('$')[1])
+	# 		elif client_pos > 0: # This transaction is an gain from the client
+	# 			total += int(curr_transaction.split('$')[1])
+	# 		i = self._chain[i].get_prev_ptr()
+	# 	return "$" + str(total)
